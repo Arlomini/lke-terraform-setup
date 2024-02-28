78330f0b028973231092662e5696eac0c1550065
ARMAND LOMINI <107315783+Arlomini@users.noreply.github.com>
2024-02-28 10:58:50 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -13,7 +13,7 @@ provider "linode" {
 
 //Use the linode_lke_cluster resource to create
 //a Kubernetes cluster
-resource "linode_lke_cluster" "my-cluster" {
+resource "linode_kubernetes_cluster" "my-cluster" {
     k8s_version = var.k8s_version
     label = var.label
     region = var.region
